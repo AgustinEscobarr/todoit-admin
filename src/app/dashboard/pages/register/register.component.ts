d37af20209d8cfd756432bd476f5c638b07f1bd1
AgustinEscobarr <agustinescobaring@gmail.com>
2021-12-20 15:26:16 -0300

    funciona el registro

--- src/app/dashboard/pages/register/register.component.ts
+++ src/app/dashboard/pages/register/register.component.ts
@@ -3,6 +3,8 @@ import { FormControl, FormGroup, FormGroupDirective } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { RegisterData, Rol, Vehicle } from '../../models/register-data';
+import { ModifyUserService } from '../../services/modify-user.service';
+import { UserComplete } from '../../models/user-structure';
 
 interface RolSelect{
   id:{
@@ -52,12 +54,39 @@ export class RegisterComponent   {
       isDeleted:0
     }
   ];
+  value:Rol={
+    id:0,
+    name:'',
+    isDeleted:0
+  };
  
-  user= new RegisterData();
+  
+  userSend :UserComplete={
+    id:0,
+    fullName:'' ,
+      email:'' ,
+      cellPhone: '',
+      address: '',
+      password: '',
+      isAccepted:true,
+      isDeleted:false,
+      observations:'',
+      vehicle:{
+        id:0,
+        name:'',
+        isDeleted:0,
+      } ,
+      rol : {
+        id:0,
+        name:'',
+        isDeleted:0
+
+      }
+  };
   
   registerForm: FormGroup;
 
-  constructor(private route:Router, public dialog:MatDialog) { 
+  constructor(private route:Router, public dialog:MatDialog, private modifyUserService:ModifyUserService) { 
     this.registerForm  = new FormGroup({
       fullName: new FormControl(),
       email: new FormControl(),
@@ -78,14 +107,25 @@ export class RegisterComponent   {
     
  
    
-    this.user=this.registerForm.value;
-    this.user.isAccepted=true;
-    this.user.isDeleted=false
+    this.userSend=this.registerForm.value;
+    this.userSend.cellPhone=this.registerForm.value.cellPhone.toString();
+    this.userSend.isAccepted=true;
+    this.userSend.isDeleted=false;
+    this.userSend.id=0;
     
     
-    console.log(this.user);
+    
+    console.log(this.userSend);
     formDirective.resetForm();
     this.registerForm.reset();
+    this.modifyUserService.modifyUser(this.userSend).subscribe(resp=>{
+      console.log(resp);
+      alert('usuario creado con exito');
+
+    },
+    error=>{
+      alert('No se pudo registrar el ususario correctamente')
+    })
     
        
       
